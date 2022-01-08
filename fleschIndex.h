3d622d8d7ae482f513a0cf2fa814cb1e5af58f44
Mohammed Abdallah <97360975+mabdal03@users.noreply.github.com>
2022-01-08 16:39:29 -0500

    Update fleschIndex.h

--- fleschIndex.h
+++ fleschIndex.h
@@ -1,17 +1,6 @@
 /*
-Student Name: Mohammed Abdallah
-Student ID: 1163393
-Due Date: February 19th, 2021
-Course: CIS*2500
-I have exclusive control over this submission via my password.
-By including this header comment, I certify that:
-1) I have read and understood the policy on academic integrity.
-2) I have completed Moodle's module on academic integrity.
-3) I have achieved at least 80% on the academic integrity quiz.
-I assert that this work is my own. I have appropriate acknowledged
-any and all material that I have used, be it directly quoted or
-paraphrased. Furthermore, I certify that this assignment was written
-by me in its entirety.
+By Mohammed Abdallah
+Last Updated on February 19th, 2021
 */
 
 //importing libraries
